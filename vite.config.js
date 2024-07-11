04587190e3718cd6cc5d8042952e9a90157e20f3
eyo-as <eyosiyastumiso7@gmail.com>
2024-07-11 20:51:34 +0300

    env update

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,16 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   base: "/Netflix-Clone/netflix-clone",
   plugins: [react()],
+  define: {
+    "process.env": process.env,
+  },
 });
+
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//     base: "/Netflix-Clone-Using-React/",
+//     plugins: [react()],
+
+// });
